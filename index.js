7d24915bad2aef7f97ad993b64c933f6aba01d9b
donbjarkone <44224869+donbjarkone@users.noreply.github.com>
2022-11-15 19:56:05 +0100

    details

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ let extract = require('pdf-text-extract');
 // 1. Extract data to array
 // 2. Parse
 
-// let PDFpath = path.join(__dirname, "pdfs/");
-let PDFpath = path.join(__dirname, "test/"); // absolute path for pdfs
+let PDFpath = path.join(__dirname, "pdfs/"); // absolute path for pdfs
 let textPath = path.join(__dirname, "texts/"); // absolute path for extracted text
 
 let extractOptions = { //options object for extract() -- doesn't seem to work properly
@@ -14,25 +13,38 @@ let extractOptions = { //options object for extract() -- doesn't seem to work pr
     lastPage: 1
 }
 
-// Gets all filenames in the pdf folder
-let filenames = fs.readdirSync(PDFpath); 
-
-for (let filename of filenames) {
-    // absolute path for the file
-    let filePath = path.join(PDFpath, filename);
+function run() {
     
-    // the callback gives text: an array of pages of the original PDF
-    extract(filePath, extractOptions, function(err, text) {
-        if (err) {
-            console.dir(err);
-            return;
-        }
-
-        writeToFiles(filename, text);
-    })
+    readPDFs();
 }
 
-function writeToFiles(originalFilename, pages) {
+run();
+
+
+/////////////////////////////////
+
+function readPDFs () {
+    // Gets all filenames in the pdf folder
+    let filenames = fs.readdirSync(PDFpath); 
+
+    for (let filename of filenames) {
+        // absolute path for the file
+        let filePath = path.join(PDFpath, filename);
+        
+        // the callback gives text: an array of pages of the original PDF
+        extract(filePath, extractOptions, function(err, text) {
+            if (err) {
+                console.dir(err);
+                return;
+            }
+
+            writeToFiles(filename, text);
+        })
+    }
+}
+
+
+function writeToFiles(originalFilename, pageArray) {
     // Get year from filename and build new folder path
     let year = originalFilename.match(/\d{4}/)[0];
     let localDir = path.join(textPath, year);
@@ -43,13 +55,13 @@ function writeToFiles(originalFilename, pages) {
     }
     
     // For each page in the PDF
-    for (let i = 0; i < pages.length; i++) {
+    for (let i = 0; i < pageArray.length; i++) {
         // Path
         let name = year + "-page-" + i + ".txt";
         let pageFilename = path.join(localDir, name);
         
         // Create a new file for each page
-        fs.writeFile(pageFilename, pages[i], "utf-8", function(err) {
+        fs.writeFile(pageFilename, pageArray[i], "utf-8", function(err) {
             if (err) { console.log(err) }
         })
     }
